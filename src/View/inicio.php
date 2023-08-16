538db78e7dd26ff8eb85f6d60534803fb3c45156
Angel SD <69773924+AtkarMX@users.noreply.github.com>
2023-08-16 01:29:55 -0600

    Version 2 - Inicio.php
    + Se rediseñaron las secciones dentro de inicio.
    + Se agregaron animaciones.
    + Se pusieron textos generados por ChatGPT que representen el contenido de cada sección.
    + Se cambiaron las imágenes e iconos.

--- src/View/inicio.php
+++ src/View/inicio.php
@@ -14,7 +14,7 @@
         <img src="img/IBQ-IQ/P49.webp" class="d-block w-100 align-middle" width="100%" height="100%">
         <div class="container">
           <div class="carousel-caption d-flex flex-column">
-            <h1 class="mainTitle text-center font-bold m-2 shadow-text">Ingeniería Química</h1>
+            <h1 class="mainTitle text-center font-bold m-2 shadow-text">UNIDA</h1>
             <h2 class="mainSubtitle text-center font-bold shadow-text">Reconocimiento con acreditaciones internacionales</h1>
               <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=3">Explorar</a>
               </p>
@@ -26,7 +26,7 @@
         <img src="img/IBQ-IQ/P50.webp" class="d-block w-100 align-middle" width="100%" height="100%">
         <div class="container">
           <div class="carousel-caption">
-            <h1 class="mainTitle text-center font-bold m-2 shadow-text">Ingeniería Química</h1>
+            <h1 class="mainTitle text-center font-bold m-2 shadow-text">UNIDA</h1>
             <h2 class="mainSubtitle text-center font-bold shadow-text">Participa en la tecnología e innovación</h1>
               <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=1">Explorar</a></p>
           </div>
@@ -37,7 +37,7 @@
         <img src="img/IBQ-IQ/P39.webp" class="d-block w-100 align-middle" width="100%" height="100%">
         <div class="container">
           <div class="carousel-caption">
-            <h1 class="mainTitle text-center font-bold m-2 shadow-text">Ingeniería Química</h1>
+            <h1 class="mainTitle text-center font-bold m-2 shadow-text">UNIDA</h1>
             <h2 class="mainSubtitle text-center font-bold d-none d-md-inline-flex shadow-text">Impacto positivo para la sociedad</h1>
             <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=5">Explorar</a></p>
           </div>
@@ -60,6 +60,7 @@
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
+    
     <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
@@ -68,260 +69,134 @@
 </section>
 <!--Fin Carrusel de portada -->
 
-<!-- Sobre el posgrado -->
+<!-- Sección: Descripción de la maestría -->
 <section class="seccionElemento">
   <div class="seccion-contenedor">
 
-    <img class="seccion-imagen" src="img/IBQ-IQ/IMG2.webp" alt="">
+    <img class="seccion-imagen" src="img/Actualizacion/IMG_7594.jpg" alt="">
 
     <div class="seccion-descripcion">
       <h2 class="seccion-descripcion-titulo">Maestría en Ciencias en Ingeniería Bioquímica</h2>
       <div class="seccion-descripcion-separador"></div>
-      <p class="seccion-descripcion-texto">Como investigador, deberá ser capaz de presentar los resultados de sus investigaciones en foros científicos y en revistas de prestigio internacional bajo arbitrio, hacerse llegar fondos para sus proyectos de investigación y vincularse con los diferentes sectores productivo, académico, social, entre otros. Como formador de recursos humanos, el egresado deberá tener la habilidad para dirigir, asesorar y entrenar al personal a su cargo en los diferentes aspectos de su área de desarrollo. El egresado será un profesionista que impacte de forma sustentable en el desarrollo social de su entorno.</p>
-      <a class="seccion-descripcion-boton">Explorar</a>
+      <p class="seccion-descripcion-texto">Desarrolla tu pasión por la biotecnología y la innovación inscribiéndote en nuestra Maestría en Ciencias en Ingeniería Bioquímica. Atrévete a explorar las maravillas de la ingeniería molecular y la transformación de la biología en soluciones prácticas. Únete a nosotros para ampliar tus horizontes, contribuir al progreso científico y convertirte en un líder en la creciente industria de la bioingeniería."</p>
+      <a class="seccion-descripcion-boton">Saber más</a>
     </div>
   </div>
 </section>
 
-<!-- Lineas de investigación -->
+<!-- Sección: Lineas de investigación -->
 <section class="seccionElemento">
   <div class="seccion-contenedor">
 
-    <img class="seccion-imagen" src="img/ITVER/escuela21.webp" alt="">
+    <img class="seccion-imagen" src="img/Actualizacion/IMG_7572.jpg" alt="">
 
-    <ul class="seccion-lista">
+    <div class="seccion-lista">
       <h2 class="seccion-descripcion-titulo">Líneas de investigación</h2>
       <div class="seccion-descripcion-separador"></div>
-      <li class="seccion-lista-item">
-        <img class="seccion-lista-icono" src="img/topicos/sustentabilidadSostenibilidad.png" alt="">
-        <h5 class="seccion-lista-titulo">Maestría en Ciencias en Ingeniería Bioquímica</h5>
-      </li>
-      <li class="seccion-lista-item">
-        <img class="seccion-lista-icono" src="img/topicos/sustentabilidadSostenibilidad.png" alt="">
-        <h5 class="seccion-lista-titulo">Maestría en Ciencias en Ingeniería Bioquímica</h5>
-      </li>
-      <li class="seccion-lista-item">
-        <img class="seccion-lista-icono" src="img/topicos/sustentabilidadSostenibilidad.png" alt="">
-        <h5 class="seccion-lista-titulo">Maestría en Ciencias en Ingeniería Bioquímica</h5>
-      </li>
-    </ul>
+        <a href="?option=3" class="seccion-lista-item">
+          <img class="seccion-lista-icono" src="img/topicos/procesosIndustriales.png" alt="">
+          <h5 class="seccion-lista-titulo">Aplicaciones de la Biología Molecular en Ingeniería Metabólica, Biocatálisis y Control Biológico</h5>
+        </a>
+
+        <a href="?option=3" class="seccion-lista-item">
+          <img class="seccion-lista-icono" src="img/topicos/controlProcesos.png" alt="">
+          <h5 class="seccion-lista-titulo">Estudios Nutrigenéticos, Funcionales y toxicológicos de Alimentos</h5>
+        </a>
+
+        <a href="?option=3" class="seccion-lista-item">
+          <img class="seccion-lista-icono" src="img/topicos/riesgosIndustriales.png" alt="">
+          <h5 class="seccion-lista-titulo">Ingeniería y Tecnología de Procesos Biológicos</h5>
+        </a>
+
+        <a href="?option=3" class="seccion-lista-item">
+          <img class="seccion-lista-icono" src="img/topicos/formulacionProyectos.png" alt="">
+          <h5 class="seccion-lista-titulo">Tecnología y Nanotecnología de Alimentos</h5>
+        </a>
+
+        <a href="?option=3" class="seccion-lista-item">
+          <img class="seccion-lista-icono" src="img/topicos/analisisQuimico.png" alt="">
+          <h5 class="seccion-lista-titulo">Ingeniería de Alimentos</h5>
+        </a>
+    </div>
   </div>
 </section>
 
-<!-- Instalaciones -->
+<!-- Sección: Instalaciones -->
 <section class="seccionElemento modoOscuro">
   <div class="seccion-contenedor">
 
     <div class="seccion-descripcion">
       <h2 class="seccion-descripcion-titulo">Instalaciones</h2>
       <div class="seccion-descripcion-separador"></div>
-      <p class="seccion-descripcion-texto">Como investigador, deberá ser capaz de presentar los resultados de sus investigaciones en foros científicos y en revistas de prestigio internacional bajo arbitrio, hacerse llegar fondos para sus proyectos de investigación y vincularse con los diferentes sectores productivo, académico, social, entre otros. Como formador de recursos humanos, el egresado deberá tener la habilidad para dirigir, asesorar y entrenar al personal a su cargo en los diferentes aspectos de su área de desarrollo. El egresado será un profesionista que impacte de forma sustentable en el desarrollo social de su entorno.</p>
+      <p class="seccion-descripcion-texto">Te invitamos a explorar los laboratorios que conforman nuestra Maestría en Ciencias en Ingeniería Bioquímica. Cada laboratorio ha sido meticulosamente diseñado para sumergirte en el fascinante mundo de la biotecnología y la ingeniería molecular. Desde la manipulación de biomoléculas hasta la producción de biofármacos, nuestros laboratorios ofrecen una plataforma única para poner en práctica tus habilidades y contribuir a la vanguardia de la investigación científica.</p>
       <a class="seccion-descripcion-boton">Explorar</a>
     </div>
 
     <div class="seccion-galeria">
-      <img class="seccion-imagen" src="img/ITVER/escuela21.webp" alt="">
-      <img class="seccion-imagen" src="img/ITVER/escuela21.webp" alt="">
-      <img class="seccion-imagen" src="img/ITVER/escuela21.webp" alt="">
-      <img class="seccion-imagen" src="img/ITVER/escuela21.webp" alt="">
+      <img class="seccion-imagen" src="img/Actualizacion/IMG_7573.jpg" alt="">
+      <img class="seccion-imagen" src="img/Actualizacion/IMG_7431.jpg" alt="">
+      <img class="seccion-imagen" src="img/Actualizacion/IMG_7502.jpg" alt="">
+      <img class="seccion-imagen" src="img/Actualizacion/IMG_7486.jpg" alt="">
     </div>
   </div>
 </section>
 
-<!-- Areas de aprendizaje -->
-<section class="lightSection bg-light p-2">
-  <h2 class="sectionTitle text-center font-bold m-3">Áreas de aprendizaje</h2>
-  <div class="sectionSeparator"></div>
+<!-- Sección: Comunidad docente -->
+<section class="seccionElemento">
+  <div class="seccion-contenedor">
 
-  <div class="row g-0">
 
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3">
-          <img class="areaImage items-center" src="img/topicos/procesosIndustriales.png" alt="">
-        </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Diseño y Optimización de Procesos Industriales</h3>
-        <p class="areaText">
-        Diseña, desarrolla y optimiza procesos en industrias químicas y de servicios.
-        </p>
-      </div>
+    <div class="seccion-galeria">
+      <img class="seccion-imagen-pequeña" src="img/Docentes/sandraPerezCorrea.webp" alt="">
+      <img class="seccion-imagen-pequeña" src="img/Docentes/genovevaDominguezSanchez.webp" alt="">
+      <img class="seccion-imagen-pequeña" src="img/Docentes/manuelSusunagaMiranda.webp" alt="">
+      <img class="seccion-imagen-pequeña" src="img/Docentes/joseBarranHerrera.webp" alt="">
+      <img class="seccion-imagen-pequeña" src="img/Docentes/patriciaMendozaGarcia.webp" alt="">
+      <img class="seccion-imagen-pequeña" src="img/Docentes/aniluMirandaMedina.webp" alt="">
     </div>
 
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3">
-          <img class="areaImage items-center" src="img/topicos/simulacionProcesos.png" alt="">
-        </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Simulación de Procesos</h3>
-        <p class="areaText">
-        Simula procesos químicos mediante el uso de software comercial.
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3">
-          <img class="areaImage items-center" src="img/topicos/sustentabilidadSostenibilidad.png" alt="">
-        </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Sustentabilidad y Sostenibilidad.</h3>
-        <p class="areaText">
-        Contribuye al cumplimiento de los objetivos del desarrollo sostenible, 
-        con el desarrollo de procesos sostenibles en equilibrio social, ambiental y económico.
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3">
-          <img class="areaImage items-center" src="img/topicos/controlProcesos.png" alt="">
-        </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Control de Procesos</h3>
-        <p class="areaText">
-        Selecciona y aplica la instrumentación para el control de procesos químicos a partir de 
-        los requerimientos óptimos de operación.
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3">
-          <img class="areaImage items-center" src="img/topicos/riesgosIndustriales.png" alt="">
-        </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Salud y Riesgo en los Procesos</h3>
-        <p class="areaText">
-        Realiza análisis de riesgo con el fin de definir las acciones de seguridad funcional y 
-        establecer programas de prevención y control de incidentes en plantas de procesos. 
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3">
-          <img class="areaImage items-center" src="img/topicos/formulacionProyectos.png" alt="">
-        </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Formulación y Evaluación de Proyectos</h3>
-        <p class="areaText">
-        Obtendrás conocimientos en seguridad industrial y medio ambiente, incluyendo la gestión de riesgos, 
-        el manejo adecuado de productos químicos, el cumplimiento de normas de seguridad y las consideraciones 
-        ambientales en la industria química.
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4">
-        <a href="?option=3">
-          <img class="areaImage items-center" src="img/topicos/analisisQuimico.png" alt="">
-        </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Análisis Químico </h3>
-        <p class="areaText">
-        Aprende técnicas de análisis comúnmente utilizados para el control de procesos químicos.
-        </p>
-      </div>
+    <div class="seccion-descripcion">
+      <h2 class="seccion-descripcion-titulo">Comunidad docente</h2>
+      <div class="seccion-descripcion-separador"></div>
+      <p class="seccion-descripcion-texto">Disponemos de un cuerpo docente multidisciplinario conformado por agentes de cambio, con sólida formación de posgrado y una rica trayectoria en el ámbito pedagógico. Su dedicación a la excelencia educativa se evidencia a diario en las aulas. Estos profesionales tienen la capacidad de moldear destinos, de dejar una huella profunda en las próximas generaciones de estudiantes, y de efectuar una influencia duradera en la esfera académica y más allá.</p>
+      <a class="seccion-descripcion-boton">Conocer</a>
     </div>
 
   </div>
 </section>
-<!-- Fin Áreas de aprendizaje-->
 
-<!-- Comunidad docente  -->
-<section class="p-2 bg-light">
-  <div class="row px-2 g-0">
+<!-- Sección: Recursos -->
+<section class="seccionElemento">
+  <div class="seccion-contenedor">
 
-    <div class="col-lg-6 col-sm-12">
-      <div class="galleryContainer bg-light">
-        <div class="row g-0">
-          <div class="col-4 px-2">
-            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/sandraPerezCorrea.webp"
-              alt="">
-            <img class="img-fluid w-100 shadow-1-strong rounded m-2"
-              src="img/Docentes/genovevaDominguezSanchez.webp" alt="">
-          </div>
-
-          <div class="col-4 px-2">
-            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/manuelSusunagaMiranda.webp"
-              alt="">
-            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/joseBarranHerrera.webp" alt="">
-          </div>
-
-          <div class="col-4 px-2">
-            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/patriciaMendozaGarcia.webp"
-              alt="">
-            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/aniluMirandaMedina.webp"
-              alt="">
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-    <div class="lightSection col-lg-6 col-12 px-4">
-      <div class="d-flex justify-content-center align-items-center w-100 h-100">
-        <div class="row g-0">
-          <div class="col-12">
-            <h2 class="sectionTitle text-center font-bold m-3">Comunidad docente</h2>
-            <div class="sectionSeparator"></div>
-          </div>
-          <div class="col-12" style="text-align: center;">
-            <p style="text-align: justify;">
-            Contamos con docentes de diversas disciplinas que son impulsores de cambio, 
-            con estudios de posgrado y una amplia experiencia en el quehacer docente, quienes 
-            demuestran diariamente su compromiso en las aulas. Son profesionales que tienen la facultad de 
-            transformar vidas, de impactar a las futuras generaciones de estudiantes.
-            </p>
-            <a class="btn btn-warning font-bold" href="?option=2" role="button">¡Conocer docentes!</a>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- Fin Comunidad docente -->
-
-<!-- Enlaces -->
-<section class="lightSection bg-light p-2">
-  <h2 class="sectionTitle text-center font-bold m-3">Enlaces</h2>
-  <div class="sectionSeparator"></div>
-
-  <div class="row g-0">
-
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4 rounded-3">
-        <a href="http://gestion.itver.edu.mx/servicio-social/" target="_blank" >
-          <img class="img-fluid w-100 shadow-1-strong rounded mb-4 p-1" src="img/gestionVinculacion2.webp" alt="">
+    <div class="seccion-lista">
+      <h2 class="seccion-descripcion-titulo">Recursos</h2>
+      <div class="seccion-descripcion-separador"></div>
+        <a href="#" class="seccion-lista-item">
+          <h5 class="seccion-lista-titulo">Proceso de admisión</h5>
         </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Servicio social</h3>
-      </div>
-    </div>
 
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4 rounded-3">
-        <a href="http://www.veracruz.tecnm.mx/" target="_blank" >
-          <img class="img-fluid w-100 shadow-1-strong rounded mb-4  p-1" src="img/Venus.webp" alt="">
+        <a href="#" class="seccion-lista-item">
+          <h5 class="seccion-lista-titulo">Formato de resumen</h5>
         </a>
-        <h3 class="areaTitle text-center font-bold text-xl">TECNM Veracruz</h3>
-      </div>
-    </div>
 
-    <div class="col-lg-4 col-sm-6 text-center p-3">
-      <div class="area shadow-sm p-4 rounded-3">
-        <a href="?option=5">
-          <img class="img-fluid w-90 shadow-1-strong rounded mb-4  p-1" src="img/comunidadIQ.webp" alt="">
+        <a href="#" class="seccion-lista-item">
+          <h5 class="seccion-lista-titulo">Formato de tesis</h5>
         </a>
-        <h3 class="areaTitle text-center font-bold text-xl">Comunidades</h3>
-      </div>
+
+        <a href="#" class="seccion-lista-item">
+          <h5 class="seccion-lista-titulo">Plan de estudios</h5>
+        </a>
+
+        <a href="#" class="seccion-lista-item">
+          <h5 class="seccion-lista-titulo">Procesos de reinscripción</h5>
+        </a>
     </div>
 
-    
+    <img class="seccion-imagen" src="img/Actualizacion/IMG_7462.jpg" alt="">
 
   </div>
 </section>
-<!-- Fin Enlaces ->
 
 <!-- Área de redes sociales -->
 <section class="lightSection p-2 g-0">
