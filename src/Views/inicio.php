dacb9a1ac600ecab04539030fba1640c104e47ae
Angel SD <69773924+AtkarMX@users.noreply.github.com>
2023-08-22 21:07:44 -0600

    Version 3 - Inicio.php
    + Se modifico la sección de redes sociales en Inicio para que este más acorde al nuevo diseño, además se agregaron animaciones.

--- src/Views/inicio.php
+++ src/Views/inicio.php
@@ -193,124 +193,26 @@
 <section class="seccion-contenedor">
 
   <div class="seccion-lista">
-    <h2 class="seccion-texto-titulo">Recursos</h2>
+    <h2 class="seccion-texto-titulo">Redes sociales</h2>
     <div class="seccion-texto-separador"></div>
-    <a href="#" class="seccion-lista-item">
-      <h5 class="seccion-lista-parrafo">Proceso de admisión</h5>
-    </a>
 
-    <a href="#" class="seccion-lista-item">
-      <h5 class="seccion-lista-parrafo">Formato de resumen</h5>
-    </a>
+    <h3 class="seccion-texto-subtitulo">Tecnológico Nacional de México</h3>
+    <div class="seccion-redes">
+      <a href="https://www.tecnm.mx"><img class="seccion-redes-icono" src="img/iconos/tecnm-logo.PNG" alt=""></a>
+      <a href="https://www.facebook.com/TecNMmx"><img class="seccion-redes-icono" src="img/iconos/facebook.PNG" alt=""></a>
+      <a href="https://twitter.com/TecNM_MX"><img class="seccion-redes-icono" src="img/iconos/twitter.PNG" alt=""></a>
+      <a href="https://www.youtube.com/user/SNESTMX"><img class="seccion-redes-icono" src="img/iconos/youtube.PNG" alt=""></a>
+    </div>
 
-    <a href="#" class="seccion-lista-item">
-      <h5 class="seccion-lista-parrafo">Formato de tesis</h5>
-    </a>
-
-    <a href="#" class="seccion-lista-item">
-      <h5 class="seccion-lista-parrafo">Plan de estudios</h5>
-    </a>
-
-    <a href="#" class="seccion-lista-item">
-      <h5 class="seccion-lista-parrafo">Procesos de reinscripción</h5>
-    </a>
+    <h3 class="seccion-texto-subtitulo">Instituto Tecnológico de Veracruz</h3>
+    <div class="seccion-redes">
+      <a href="https://www.veracruz.tecnm.mx/index.php"><img class="seccion-redes-icono" src="img/iconos/itver-logo.PNG" alt=""></a>
+      <a href="https://www.facebook.com/ITVeracruz"><img class="seccion-redes-icono" src="img/iconos/facebook.PNG" alt=""></a>
+      <a href="https://twitter.com/tvitver"><img class="seccion-redes-icono" src="img/iconos/twitter.PNG" alt=""></a>
+      <a href="https://www.youtube.com/channel/UCVBkOP_rj5ulvlriG88jTzA"><img class="seccion-redes-icono" src="img/iconos/youtube.PNG" alt=""></a>
+    </div>
   </div>
 
-  <img class="seccion-imagen" src="img/laboratorios/IMG_7462.jpg" alt="">
+  <img class="seccion-imagen" src="img/laboratorios/fisico-quimica/IMG_7420.jpg" alt="">
 
 </section>
-
-<!-- Área de redes sociales -->
-<section class="lightSection p-2 g-0">
-  <div class="row g-0">
-    <div class="col">
-      <h2 class="sectionTitle text-center font-bold m-3">Redes sociales</h2>
-      <div class="sectionSeparator"></div>
-    </div>
-  </div>
-</section>
-<section class="p-2 g-0">
-  <div class="row g-0">
-    <div class="col-12 col-md-6">
-
-      <h4 class="sectionTitle text-center font-bold">Tecnológico Nacional de México</h4>
-
-      <div class="d-flex flex-row justify-content-center">
-        <a class="mx-2" href="https://www.tecnm.mx/" target="_blank" style="text-decoration: none;">
-          <div class="d-flex flex-column justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px"
-              src="img/iconos/tecnm-logo.PNG" alt="">
-          </div>
-        </a>
-        <a class="mx-2" href="https://www.facebook.com/TecNMmx" target="_blank" style="text-decoration: none;">
-          <div class="d-flex flex-row justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/facebook.PNG"
-              alt="">
-          </div>
-        </a>
-        <a class="mx-2" href="https://twitter.com/TecNM_MX" target="_blank" style="text-decoration: none;">
-          <div class="d-flex flex-row justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/twitter.PNG"
-              alt="">
-          </div>
-        </a>
-        <a class="mx-2" href="https://www.youtube.com/user/SNESTMX" target="_blank" style="text-decoration: none;">
-          <div class="d-flex flex-row justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/youtube.PNG"
-              alt="">
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="col-12 col-md-6">
-      <h4 class="sectionTitle  text-center font-bold">Instituto Tecnológico de Veracruz</h4>
-      <div class="d-flex flex-row justify-content-center">
-        <a class="mx-2" href="https://www.veracruz.tecnm.mx/index.php" target="_blank" style="text-decoration: none;">
-          <div class="d-flex flex-row justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px"
-              src="img/iconos/itver-logo.PNG" alt="">
-          </div>
-        </a>
-        <a class="mx-2" href="https://www.facebook.com/ITVeracruz" target="_blank" style="text-decoration: none;">
-          <div class="d-flex flex-row justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/facebook.PNG"
-              alt="">
-          </div>
-        </a>
-        <a class="mx-2" href="https://twitter.com/tvitver" target="_blank" style="text-decoration: none;">
-          <div class="d-flex flex-row justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/twitter.PNG"
-              alt="">
-          </div>
-        </a>
-        <a class="mx-2" href="https://www.youtube.com/channel/UCVBkOP_rj5ulvlriG88jTzA" target="_blank"
-          style="text-decoration: none;">
-          <div class="d-flex flex-row justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/youtube.PNG"
-              alt="">
-          </div>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="row g-0">
-    <h4 class="sectionTitle  text-center font-bold">Departamento de Ingeniería Química - Bioquímica</h4>
-    <div class="d-flex flex-row justify-content-center">
-      <a class="mx-2" href="https://www.facebook.com/groups/itveringiq/" target="_blank"
-        style="text-decoration: none;">
-        <div class="d-flex flex-row justify-content-center">
-          <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="width: 40px" src="img/iconos/facebook.PNG"
-            alt="">
-        </div>
-      </a>
-      <a class="mx-2" href="https://www.youtube.com/channel/UC3-FcFXHHyqRLVAxbo3AM9A" target="_blank"
-          style="text-decoration: none;">
-          <div class="d-flex flex-row justify-content-center">
-            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/youtube.PNG"
-              alt="">
-          </div>
-        </a>
-    </div>
-  </div>
-</section>
-<!-- Fin Área de redes sociales -->
