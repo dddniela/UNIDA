167361a65ac949ec815fc03951214a3a232e7bd3
Daniela Castro <daniela.castro@phs.mx>
2023-10-01 23:50:20 -0600

    docentes

--- src/Views/plantillaDocente-vista.php
+++ src/Views/plantillaDocente-vista.php
@@ -18,13 +18,13 @@
   <div class="row justify-content-md-start justify-content-center">
     <?php
   
-            $inf = 1;
-            $sup = 12;
+    
+            $inf = 0;
             if(isset($_GET['inferior'])){
                 $inf = $_GET['inferior'];
-                $sup = $_GET['superior'];
             }
-            $datos = $docente->imprimirDatos($inf,$sup);
+            
+            $datos = $docente->imprimirDatos($inf);
             echo $datos;
         ?>
   </div>
