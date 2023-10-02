657df71401008ce2b631cdc9faa4e39af44faaaf
MarckGabriel2 <73321649+MarckGabriel2@users.noreply.github.com>
2023-10-02 00:13:11 -0600

    actualizacion banner docente

--- src/Views/plantillaDocente-vista.php
+++ src/Views/plantillaDocente-vista.php
@@ -1,18 +1,16 @@
+<!-- Sección: Portada -->
 <div class="row g-0">
-  <div class="position-relative w-100 overflow-hidden">
-    <img class="w-100 img-fluid" src="img/IBQ-IQ/P15.webp" alt="" />
-    <div class="position-absolute top-50 start-50 translate-middle w-100">
-      <div
-        class="d-flex flex-column justify-content-center align-items-center text-center"
-      >
-        <h1 class="fw-bold text-warning shadow-text">Plantilla Docente</h1>
-        <h1 class="fw-bold text-light d-md-flex d-none shadow-text">
-          Ingeniería Química
-        </h1>
-      </div>
+    <div class="position-relative w-100 overflow-hidden">
+        <img class="w-100 img-fluid" src="img/banner1.webp" alt="">
+        <div class="position-absolute top-50 start-50 translate-middle w-100">
+            <div class="d-flex flex-column justify-content-center align-items-center text-center">
+                <h1 class="fw-bold text-warning shadow-text">Núcleo Académico</h1>
+                <h1 class="fw-bold text-light d-md-flex d-none shadow-text">Maestría en Ciencias en Ingeniería Bioquímica</h1>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
+
 
 <div class="container my-2">
   <div class="row justify-content-md-start justify-content-center">
